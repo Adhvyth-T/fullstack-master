428c1d269408eaff503b2933fb791339a0fa43fc
Adhvyth-T <150608959+Adhvyth-T@users.noreply.github.com>
2025-09-18 11:48:53 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,8 @@
-output "frontend_service_ip" {
-  description = "Public IP of the frontend service"
-  value       = kubernetes_service.frontend.status[0].load_balancer[0].ingress[0].ip
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config[0].raw_kube_config
+  sensitive = true
+}
+
+output "aks_name" {
+  value = azurerm_kubernetes_cluster.aks.name
 }
