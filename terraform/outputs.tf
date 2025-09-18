d92e3e5a0dcf3d3af35dd496b5e61fa6d9e169f3
Adhvyth-T <150608959+Adhvyth-T@users.noreply.github.com>
2025-09-18 11:58:11 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,15 @@
+# Full kubeconfig as string
 output "kube_config" {
-  value     = azurerm_kubernetes_cluster.aks.kube_config[0].raw_kube_config
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
   sensitive = true
 }
 
+# Frontend LoadBalancer IP
+output "frontend_service_ip" {
+  value = kubernetes_service.frontend.status[0].load_balancer[0].ingress[0].ip
+}
+
+# AKS cluster name
 output "aks_name" {
   value = azurerm_kubernetes_cluster.aks.name
 }
