f8b55b559f95bf644415c7ba2b1037722027530a
Adhvyth-T <150608959+Adhvyth-T@users.noreply.github.com>
2025-09-18 12:11:16 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,12 +1,15 @@
-# Full kubeconfig as string
+# Full kubeconfig
 output "kube_config" {
   value     = azurerm_kubernetes_cluster.aks.kube_config_raw
   sensitive = true
 }
 
-# Frontend LoadBalancer IP
+# Frontend LoadBalancer IP (waits for ingress if not ready immediately)
 output "frontend_service_ip" {
-  value = kubernetes_service.frontend.status[0].load_balancer[0].ingress[0].ip
+  value = try(
+    kubernetes_service.frontend.status[0].load_balancer[0].ingress[0].ip,
+    "Pending"
+  )
 }
 
 # AKS cluster name
